1d1c22beda645e803f766b440ffbccba062e3576
Zhu Shengqi <zetasq@gmail.com>
2017-10-16 21:51:28 +0800

    Setup project

---
+++ SwiftyDefaults-iOS/SwiftyDefaults_iOS.h
@@ -0,0 +1,18 @@
+//
+//  SwiftyDefaults_iOS.h
+//  SwiftyDefaults-iOS
+//
+//  Created by Zhu Shengqi on 16/10/2017.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyDefaults_iOS.
+FOUNDATION_EXPORT double SwiftyDefaults_iOSVersionNumber;
+
+//! Project version string for SwiftyDefaults_iOS.
+FOUNDATION_EXPORT const unsigned char SwiftyDefaults_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyDefaults_iOS/PublicHeader.h>
+
+
---
+++ SwiftyDefaults-macOS/SwiftyDefaults_macOS.h
@@ -0,0 +1,18 @@
+//
+//  SwiftyDefaults_macOS.h
+//  SwiftyDefaults-macOS
+//
+//  Created by Zhu Shengqi on 16/10/2017.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftyDefaults_macOS.
+FOUNDATION_EXPORT double SwiftyDefaults_macOSVersionNumber;
+
+//! Project version string for SwiftyDefaults_macOS.
+FOUNDATION_EXPORT const unsigned char SwiftyDefaults_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyDefaults_macOS/PublicHeader.h>
+
+
